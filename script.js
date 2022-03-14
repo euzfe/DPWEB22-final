a08c7a191fdb6a480355a594dc06ccc754187df3
euzfe <euzfernanda@gmail.com>
2022-03-14 10:59:29 -0300

    Projeto atualizado

---
+++ script.js
@@ -0,0 +1,9 @@
+new Glider(document.querySelector('.glider'), {
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    rewind: true,
+    arrows: {
+      prev: '.glider-prev',
+      next: '.glider-next'
+    }
+  });
