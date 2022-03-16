99e1b417fc73a2d75ef82784b5b0eb766ebd7b2f
euzfe <euzfernanda@gmail.com>
2022-03-16 10:51:55 -0300

    Projeto atualizado

--- script.js
+++ script.js
@@ -1,9 +1,40 @@
 new Glider(document.querySelector('.glider'), {
-    slidesToShow: 4,
-    slidesToScroll: 1,
-    rewind: true,
-    arrows: {
-      prev: '.glider-prev',
-      next: '.glider-next'
-    }
-  });
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  rewind: true,
+  arrows: {
+    prev: '.glider-prev',
+    next: '.glider-next'
+  }
+});
+
+new Glider(document.querySelector('.glider2'), {
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  rewind: true,
+  arrows: {
+    prev: '.glider-prev2',
+    next: '.glider-next2'
+  }
+});
+
+new Glider(document.querySelector('.glider3'), {
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  rewind: true,
+  arrows: {
+    prev: '.glider-prev3',
+    next: '.glider-next3'
+  }
+});
+
+new Glider(document.querySelector('.glider4'), {
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  rewind: true,
+  arrows: {
+    prev: '.glider-prev4',
+    next: '.glider-next4'
+  }
+});
+
